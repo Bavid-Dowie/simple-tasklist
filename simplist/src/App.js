734203d87b8e19c4cb421fa37c5d02ae4b4bc20d
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-12-10 14:39:55 -0500

    initial app setup

---
+++ simplist/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Checklist from './components/Checklist';
+import './styles/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Jeremy's Daily Tasklist</h1>
+      <Checklist />
+    </div>
+  );
+}
+
+export default App;
